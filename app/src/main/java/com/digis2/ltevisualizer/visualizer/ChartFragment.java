7373a1ecbbe9ef8a6afac23949d83363dc8938eb
AmmarRabie <ammaralsayed55@gmail.com>
2021-01-02 13:40:46 +0200

    Use line chart and display the data in realtime

--- app/src/main/java/com/digis2/ltevisualizer/visualizer/ChartFragment.java
+++ app/src/main/java/com/digis2/ltevisualizer/visualizer/ChartFragment.java
@@ -1,29 +1,44 @@
 package com.digis2.ltevisualizer.visualizer;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.digis2.ltevisualizer.R;
 import com.digis2.ltevisualizer.common.IMetricsObserver;
 import com.digis2.ltevisualizer.common.model.LTEMetricsModel;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
 import androidx.fragment.app.Fragment;
 
 /**
  * Responsible for rendering the metrics line chart, container
  */
-public class ChartFragment extends Fragment implements IMetricsObserver {
+public class ChartFragment extends Fragment implements IMetricsObserver, OnChartValueSelectedListener {
     private static final String TAG = "ChartFragment";
-
+    private final String[] names = {"SINR", "RSRP", "RSRQ"};
+    private final int[] colors = ColorTemplate.VORDIPLOM_COLORS;
     private ViewGroup rootView;
+    private LineChart chart;
+    private Typeface tfLight = Typeface.SERIF; // (getAssets(), "OpenSans-Light.ttf");
 
     public ChartFragment() {
         // Required empty public constructor
     }
-
 
     public static ChartFragment newInstance(String param1, String param2) {
         return new ChartFragment();
@@ -40,14 +55,131 @@ public class ChartFragment extends Fragment implements IMetricsObserver {
         // Inflate the layout for this fragment
         View parent = inflater.inflate(R.layout.fragment_chart, container, false);
         rootView = (ViewGroup) parent.findViewById(R.id.frg_chart_container);
+        init();
         return parent;
     }
 
+    private void init() {
+        chart = rootView.findViewById(R.id.metrics_chart);
+        chart.setOnChartValueSelectedListener(this);
+
+        // enable description text
+        chart.getDescription().setEnabled(true);
+
+        // enable touch gestures
+        chart.setTouchEnabled(true);
+
+        // enable scaling and dragging
+        chart.setDragEnabled(true);
+        chart.setScaleEnabled(true);
+        chart.setDrawGridBackground(false);
+
+        // if disabled, scaling can be done on x- and y-axis separately
+        chart.setPinchZoom(true);
+
+        // set an alternative background color
+        chart.setBackgroundColor(Color.LTGRAY);
+
+        LineData data = new LineData();
+        data.setValueTextColor(Color.WHITE);
+
+        // add empty data
+        chart.setData(data);
+
+        // get the legend (only possible after setting data)
+        Legend l = chart.getLegend();
+
+        // modify the legend ...
+        l.setForm(Legend.LegendForm.LINE);
+        l.setTypeface(tfLight);
+        l.setTextColor(Color.WHITE);
+
+        XAxis xl = chart.getXAxis();
+        xl.setTypeface(tfLight);
+        xl.setTextColor(Color.WHITE);
+        xl.setDrawGridLines(false);
+        xl.setAvoidFirstLastClipping(true);
+        xl.setEnabled(true);
+
+        YAxis leftAxis = chart.getAxisLeft();
+        leftAxis.setTypeface(tfLight);
+        leftAxis.setTextColor(Color.WHITE);
+//        leftAxis.s etAxisMaximum(100f);
+//        leftAxis.setAxisMinimum(0f);
+        leftAxis.setDrawGridLines(false);
+
+        YAxis rightAxis = chart.getAxisRight();
+        rightAxis.setEnabled(false);
+    }
+
+    private void addEntry(LTEMetricsModel item) {
+
+        LineData data = chart.getData();
+
+        final int[] values = {item.getSINR(), item.getRSRP(), item.getRSRQ()};
+        if (data != null) {
+
+            for (int i = 0; i < 3; i++) {
+                ILineDataSet set = data.getDataSetByIndex(i);
+                // set.addEntry(...); // can be called as well
+
+                if (set == null) {
+                    set = createSet(names[i], colors[i]);
+                    data.addDataSet(set);
+                }
+                set.addEntry(new Entry(set.getEntryCount(), (float) values[i]));
+//                data.addEntry(new Entry(set.getEntryCount(), (float) item.getRSRP()), 0);
+            }
+
+            data.notifyDataChanged();
+
+            // let the chart know it's data has changed
+            chart.notifyDataSetChanged();
+
+            // limit the number of visible entries
+            chart.setVisibleXRangeMaximum(20);
+            // chart.setVisibleYRange(30, AxisDependency.LEFT);
+
+            // move to the latest entry
+            chart.moveViewToX(data.getEntryCount());
+
+            // this automatically refreshes the chart (calls invalidate())
+            // chart.moveViewTo(data.getXValCount()-7, 55f,
+            // AxisDependency.LEFT);
+        }
+    }
+
+    private LineDataSet createSet(String name, int color) {
+
+        LineDataSet set = new LineDataSet(null, name);
+        set.setAxisDependency(YAxis.AxisDependency.LEFT);
+        set.setColor(color);
+        set.setCircleColor(Color.WHITE);
+        set.setLineWidth(2f);
+        set.setCircleRadius(4f);
+        set.setFillAlpha(65);
+        set.setFillColor(ColorTemplate.getHoloBlue());
+        set.setHighLightColor(Color.rgb(244, 117, 117));
+        set.setValueTextColor(Color.WHITE);
+        set.setValueTextSize(9f);
+        set.setDrawValues(false);
+        return set;
+    }
+
+    @Override
+    public void onValueSelected(Entry e, Highlight h) {
+        Log.i("Entry selected", e.toString());
+    }
+
+    @Override
+    public void onNothingSelected() {
+        Log.i("Nothing selected", "Nothing selected.");
+    }
+
+
     @Override
     public void onNewItem(LTEMetricsModel item) {
-        TextView newView = new TextView(getContext());
-        newView.setText(String.valueOf(item.getSINR()));
-        rootView.addView(newView);
+        addEntry(item);
     }
 
     @Override
