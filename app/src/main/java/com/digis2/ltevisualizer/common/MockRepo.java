66982ddc94a21b7d74897d35b5782e5918db3af0
AmmarRabie <ammaralsayed55@gmail.com>
2021-01-02 07:53:09 +0200

    make mock returning random data not fixed data

--- app/src/main/java/com/digis2/ltevisualizer/common/MockRepo.java
+++ app/src/main/java/com/digis2/ltevisualizer/common/MockRepo.java
@@ -13,7 +13,14 @@ import androidx.annotation.NonNull;
 public class MockRepo implements IDataService {
     @Override
     public void getRandomMetrics(@NonNull Get<LTEMetricsModel> callback) {
-        callback.onDataFetched(new LTEMetricsModel(0, 1, 2));
+//        callback.onDataFetched(new LTEMetricsModel(0, 1, 2));
+        int min = -1, max = 31;
+        int sinr = (int)(Math.random() * (max - min + 1) + min);
+        min = -111; max = -59;
+        int rsrp = (int)(Math.random() * (max - min + 1) + min);
+        min = -21; max = -2;
+        int rsrq = (int)(Math.random() * (max - min + 1) + min);
+        callback.onDataFetched(new LTEMetricsModel(rsrp, rsrq, sinr));
     }
 
     @Override
