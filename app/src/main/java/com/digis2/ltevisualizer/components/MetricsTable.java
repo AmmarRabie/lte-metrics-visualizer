10dd292370343b19285b95d6fe2d6f7f1accd6e4
AmmarRabie <ammaralsayed55@gmail.com>
2021-01-03 08:57:51 +0200

    scroll issue: support scrolling on the table

--- app/src/main/java/com/digis2/ltevisualizer/components/MetricsTable.java
+++ app/src/main/java/com/digis2/ltevisualizer/components/MetricsTable.java
@@ -2,16 +2,21 @@ package com.digis2.ltevisualizer.components;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ScrollView;
 
 import com.digis2.ltevisualizer.R;
 import com.digis2.ltevisualizer.common.model.LTEMetricsModel;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.widget.LinearLayoutCompat;
 
-public class MetricsTable extends LinearLayoutCompat {
+public class MetricsTable extends FrameLayout {
     private static final String TAG = "MetricsTable";
+    private ViewGroup contentContainerView;
 
     public MetricsTable(@NonNull Context context) {
         this(context, null);
@@ -23,15 +28,24 @@ public class MetricsTable extends LinearLayoutCompat {
     }
 
     private void init() {
-        setOrientation(VERTICAL);
+//        setOrientation(VERTICAL);
 
         // add the header from the resource file
-        inflate(getContext(), R.layout.header_metrics_table, this);
+        inflate(getContext(), R.layout.metrics_table, this);
+
+        contentContainerView = (ViewGroup) findViewById(R.id.metrics_table_content_container);
+//        ((NestedScrollView) getParent()).fullScroll(View.FOCUS_DOWN);
+    }
+
+    public void addValue(@NonNull LTEMetricsModel metrics, boolean auto_scroll) {
+        MetricsTableRow row = new MetricsTableRow(getContext());
+        row.setMetrics(metrics);
+        contentContainerView.addView(row);
+        if (auto_scroll)
+            ((ScrollView) contentContainerView.getParent()).fullScroll(View.FOCUS_DOWN);
     }
 
     public void addValue(@NonNull LTEMetricsModel metrics) {
-        MetricsTableRow row = new MetricsTableRow(getContext());
-        row.setMetrics(metrics);
-        addView(row);
+        addValue(metrics, true);
     }
 }
